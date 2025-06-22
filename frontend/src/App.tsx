4af55fcca4196089a83462e955ba72471bd02363
Caleb Yohannes <c.yohan21@outlook.com>
2025-06-22 01:48:08 -0400

    fixed component tree

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import './App.css'
 import Login from "./pages/Login"
 import Dashboard from "./pages/Dashboard"
@@ -9,14 +9,12 @@ function App() {
 
   return (
     <>
-      <Router>
         <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/upload" element={<Upload />} />
         </Routes>
-      </Router>
     </>
   )
 }
